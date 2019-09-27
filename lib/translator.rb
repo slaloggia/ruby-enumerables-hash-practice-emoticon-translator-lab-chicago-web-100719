2630719b179511b2d2b0731c01855e138ca15469
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-27 19:31:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(file, emoticon)
   library = load_library(file)
   
-  library[:get_emoticon].fetch(emoticon, "Sorry, I don't know that one.")
+  library[:get_emoticon].fetch(emoticon, "Sorry, that emoticon was not found.")
   
   
 
